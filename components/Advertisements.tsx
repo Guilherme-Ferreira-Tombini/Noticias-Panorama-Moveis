8ff75a331369ccb96f5c2e94de4ff5b1ff79bea6
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2026-09-05 21:20:42 -0300

    removido supabase e criado carrosel sem integração com o banco de dados

--- components/Advertisements.tsx
+++ components/Advertisements.tsx
@@ -1,7 +1,40 @@
-export default function Advertisemets(){
-    return(
-        <div>
-            
-        </div>
-    )
+"use client"
+
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation, Pagination } from "swiper/modules"
+
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+
+export default function Advertisements() {
+  return (
+    <div className="w-full px-5">
+      <Swiper
+        modules={[Navigation, Pagination]}
+        slidesPerView={1}
+        spaceBetween={20}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide>
+          <div className="bg-blue-500 p-10">
+            Item 1
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="bg-red-500 p-10">
+            Item 2
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="bg-green-500 p-10">
+            Item 3
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  )
 }
