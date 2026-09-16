a5724af803314e0a8c4acf89989ffa49aaa8cf3a
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2026-09-15 22:52:38 -0300

    Modificado largura dos elementos no navbar da aplicação

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import Logo from "../public/logo.png"
 export default function Navbar() {
   return (
     <nav className="w-full bg-[#1565C0] p-2 flex flex-col items-center justify-center text-white">
-      <div className="w-[85%] flex row items-center justify-between">
+      <div className="w-[90%] flex row items-center justify-between">
         <Link href={"/"}>
             <Image 
               src={Logo} 
