9c7575ddaa46bfab7ac8a989aa1ab3280e38c568
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2026-09-24 09:39:25 -0300

    Adicionado footer e ajuste no .env.example

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return(
+        <div className="w-[100%] bg-[#1565C0] p-3 flex flex-col items-center justify-center">
+            <div className="w-[90%] text-white flex flex-col items-center">
+                Guilherme Ferreira Tombini
+            </div>
+        </div>
+    )
+}
